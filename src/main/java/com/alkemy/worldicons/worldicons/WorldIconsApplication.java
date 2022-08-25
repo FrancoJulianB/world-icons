5dae7da3ce369d20174b06f11477b746e8d3754e
FrancoJulianB <Frajuba11@gmail.com>
2022-08-24 23:17:14 -0500

    Agregando relacion con base de datos

---
+++ src/main/java/com/alkemy/worldicons/worldicons/WorldIconsApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.worldicons.worldicons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldIconsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldIconsApplication.class, args);
+	}
+
+}
